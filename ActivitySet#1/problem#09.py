de4bb47d5f5a93977f8171d4a0548bc9bb7c32e0
preethi214 <97090251+preethi213@users.noreply.github.com>
2022-05-27 03:58:50 +0000

    completed

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,16 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+file_name = input("Enter file name: ")
+file_handling = open(file_name)
+empty_list = list()
+filled_list = list()
+
+for line in file_handling:
+    line = line.strip().split()
+    for word in line:
+        if word not in empty_list:
+            empty_list.append(word)
+            filled_list = sorted(empty_list)
+
+print(filled_list)
