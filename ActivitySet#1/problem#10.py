250a5ead70eec2f86c05f236c0ba8f454840187c
preethi214 <97090251+preethi213@users.noreply.github.com>
2022-05-30 15:59:07 +0000

    completed

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,21 @@
 # Dictionaries
 
 filename = "dataset/mbox-short.txt"
+f_name = input("Enter File Name: ")
+f_hand = open(f_name)
+count = dict()
+
+for line in f_hand:
+    if line.startswith("From") and not line.startswith("From:") :
+        message = line.rstrip().split()
+        for sender in message:
+            if sender == message[1]:
+                count[sender] = count.get(sender,0) + 1
+
+max_sender = None
+max_num = None
+for sender,num in count.items():
+    if max_num is None or num > max_num :
+        max_num = num
+        max_sender = sender
+print(max_sender, max_num)
