3d88449be360ac666b8ea8398e64ff99f4283d8a
preethi214 <97090251+preethi213@users.noreply.github.com>
2022-05-31 10:14:44 +0000

    completed

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,46 @@
 # Tuples
 
 filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+
+if len(name) < 1:
+
+    name = "mbox-short.txt"
+
+counts = {}
+
+handle = open(name)
+
+for line in handle:
+
+    if not line.startswith('From '):
+
+        continue
+
+    svi = line.replace(':',' ')
+
+    words = svi.split()
+
+    pu = words[5:6]
+
+    for word in pu:
+
+        counts[word] = counts.get(word,0) + 1
+
+lst = []
+
+for k,v in sorted(counts.items()):
+
+    tuk = (k,v)
+
+    lst.append(tuk)
+
+ki = sorted(lst)    
+
+for k,v in ki:
+
+    print(k,v)
+
+    
+
+
