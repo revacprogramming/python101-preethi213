b91d6f0179be4b1808835c34f8a4d199aa62c88a
preethi214 <97090251+preethi213@users.noreply.github.com>
2022-06-01 15:23:24 +0000

    recheck

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,11 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+import re
+hand=open('file12.txt')
+lst=[]
+for line in hand:
+    line = line.rstrip()
+    x = re.findall('([0-9]+)', line)
+    int_x=list(map(int,x))
+    lst.extend(int_x)
+
+total=sum(lst)
+print('Total:',total)
